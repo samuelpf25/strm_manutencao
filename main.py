71358db469dd254d984ec6225aeb35e6c7156cc5
Samuel de Paula Faria <77171431+samuelpf25@users.noreply.github.com>
2024-09-05 01:57:16 -0300

    Update main.py

--- main.py
+++ main.py
@@ -691,22 +691,88 @@ elif pg == 'Consulta':
     st.markdown(
         alerta + f'<Strong><i>Número de OS com o filtro correspondente: {len(dad[dad['area_manutencao'].str.strip() != ''])}.</i></Strong></p>',
         unsafe_allow_html=True)
-    # Formulário de filtro
     with st.form(key='form1'):
-        coluna_busca = st.selectbox('Coluna para busca por argumento', titulos)
+        tit_plan = titulos
+        coluna_busca = st.selectbox('Coluna para busca por argumento', tit_plan)
         texto = st.text_input('Busca por argumento na coluna selecionada: ')
-        btn1 = st.form_submit_button('Filtrar')
+        # col1, col2 = st.columns(2)
+        # col3, col4 = st.columns(2)
+        # col5, col6 = st.columns(2)
+        filtrar = []
+        #
+        #
+        # valor = data_solicitacao
+        # valor = list(dict.fromkeys(valor))  # removendo valores duplicados
+        # valor = sorted(valor)  # ordenando lista de string
+        # filtro_data = col1.multiselect('Filtrar por Data:', valor)
+        # if (len(filtro_data) > 0):
+        #     if (len(filtrar) > 0):
+        #         filtrar = filtrar & dados['data_solicitacao'].isin(filtro_data)
+        #     else:
+        #         filtrar = dados['data_solicitacao'].isin(filtro_data)
+        #
+        # valor = ordem_servico
+        # valor = list(dict.fromkeys(valor))  # removendo valores duplicados
+        # valor = sorted(valor)  # ordenando lista de string
+        # filtro_os = col2.multiselect('Filtrar por Ordem de Serviço:', valor)
+        # if (len(filtro_os) > 0):
+        #     if (len(filtrar) > 0):
+        #         filtrar = filtrar & dados['ordem_servico'].isin(filtro_os)
+        #     else:
+        #         filtrar = dados['ordem_servico'].isin(filtro_os)
+        # valor = nome_solicitante
+        # valor = list(dict.fromkeys(valor))  # removendo valores duplicados
+        # valor = sorted(valor)  # ordenando lista de string
+        # filtro_solicitante = col3.multiselect('Filtrar por Nome do Solicitante:', valor)
+        # if (len(filtro_solicitante) > 0):
+        #     # filtro_solicitante=valor
+        #     if (len(filtrar) > 0):
+        #         filtrar = filtrar & dados['nome_solicitante'].isin(filtro_solicitante)
+        #     else:
+        #         filtrar = dados['nome_solicitante'].isin(filtro_solicitante)
+        #
+        # valor = status_todos
+        # valor = list(dict.fromkeys(valor))  # removendo valores duplicados
+        # valor = sorted(valor)  # ordenando lista de string
+        # filtro_status = col4.multiselect('Filtrar por Status:', valor)
+        # if (len(filtro_status) > 0):
+        #     if (len(filtrar) > 0):
+        #         filtrar = filtrar & dados['status_uft'].isin(filtro_status)
+        #     else:
+        #         filtrar = dados['status_uft'].isin(filtro_status)
+        #
+        # valor = predio
+        # valor = list(dict.fromkeys(valor))  # removendo valores duplicados
+        # valor = sorted(valor)  # ordenando lista de string
+        # filtro_predio = col5.multiselect('Filtrar por localização:', valor)
+        # if (len(filtro_predio) > 0):
+        #     if (len(filtrar) > 0):
+        #         filtrar = filtrar & dados['predio'].isin(filtro_predio)
+        #     else:
+        #         filtrar = dados['predio'].isin(filtro_predio)
 
-        # Filtragem
-        if btn1:
-            if texto and coluna_busca:
-                dad = dados[dados[coluna_busca].str.contains(texto, na=False)]
-            else:
-                dad = dados
-            st.dataframe(dad)
+        btn1 = st.form_submit_button('Filtrar')
+        if (len(filtrar) == 0):
+            filtrar = titulos
+
+    if (btn1 == True):
+        # dados=df[titulos]
+        # filtrar=dados[titulo_coluna].isin([filtro])
+        # print(filtrar)
+        # if(len(filtrar)>0):
+        if (texto != '' and coluna_busca != ''):
+            dad1 = dados[filtrar][dados[coluna_busca].str.contains(texto, na=False)]
+            # dad2 = dados[filtrar][dados['Carimbo de data/hora'].str.contains(texto, na=False)]
+            dad = dad1
+        else:
+            dad = dados[filtrar]
+        try:
+            st.dataframe(dad)  # dados[filtrar].head()
             df_xlsx = to_excel(dad)
             st.download_button(label='📥 Baixar Resultado do Filtro em Excel', data=df_xlsx,
                                file_name='filtro_planilha.xlsx')
+        except:
+            pass
 
         # dados_graf=pd.DataFrame(dados[filtrar],columns=[coluna1,coluna2])
         # fig = px.bar(dados_graf, x=coluna1, y=coluna2, barmode='group', height=400)
@@ -715,7 +781,11 @@ elif pg == 'Consulta':
 
         # else:
         #    st.dataframe(df[titulos])
-
+    else:
+        try:
+            st.dataframe(df[titulos])
+        except:
+            pass
 
     try:
         df2 = dad
